5121f67159e09cc4436f003558923ba49e237bd1
Carter Bleyl <43888656+CallCab8109@users.noreply.github.com>
2026-09-08 21:46:44 -0600

    Completed all projects

--- BleylCarter_Project3_TipCalculator.py
+++ BleylCarter_Project3_TipCalculator.py
@@ -1,2 +1,8 @@
+print("Carter Bleyl Tip Calculator App") #display app name
 
-
+costOfMeal = float(input("Cost of meal: ")) #enter cost of meal
+tipPercentage = int(input("Tip percent: ")) #enter tip percentage
+print()
+tipAmount = costOfMeal * (tipPercentage / 100) # calculate tip amount
+print("Tip amount: ", round(tipAmount, 2)) #print and round tip amount
+print("Total Amount: ", round(tipAmount + costOfMeal, 2)) #print and round total amount
